3dadc7d972bdce7bdd3bb89ac3b47e9746791bea
RobertoGutierrezGuerrero <73426108+RobertoGutierrezGuerrero@users.noreply.github.com>
2022-04-02 14:13:19 +0200

    MUERTEPROGRAMADAAAAAAAA

---
+++ Assets/Scripts/MuerteProgramada.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuerteProgramada : MonoBehaviour
+{
+    [SerializeField] float tiempoDestruccion;
+    [SerializeField] PullFX.FXs tipoFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("MuerteProgramada", tiempoDestruccion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Muerte()
+    {
+        PullFX.instance.DestroyObject(this.gameObject,tipoFX);
+    }
+}
---
+++ Assets/Scripts/PullFX.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullFX : MonoBehaviour
+{
+    public enum FXs
+    {
+        explosionDisparo
+    }
+    public static PullFX instance;
+    
+    private void Awake()
+    {
+        if (!instance) instance = this;
+        else Destroy(this.gameObject);
+    }
+    [SerializeField] GameObject explosionDisparo;
+    List<GameObject> explosionesDisparos = new List<GameObject>();
+
+    public GameObject NewExplosionDisparo()
+    {
+
+        GameObject explosion = null;
+        if (explosionesDisparos.Count > 0)
+        {
+            explosion = explosionesDisparos[0];
+            explosion.SetActive(true);
+            explosionesDisparos.Remove(explosion);
+        }
+        else
+        {
+            explosion = Instantiate(explosionDisparo, transform);
+        }
+
+
+        return explosion;
+    }
+
+    public void DestroyObject(GameObject fx, FXs tipo)
+    {
+        switch (tipo)
+        {
+            case FXs.explosionDisparo:
+                DestroyExplosionDisparo(fx);
+                break;
+        }
+    }
+
+    private  void DestroyExplosionDisparo(GameObject explosion)
+    {
+        explosionesDisparos.Add(explosion);
+        explosion.SetActive(false);
+    }
+}
