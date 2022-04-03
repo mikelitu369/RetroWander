480a4ab481cda9a3b72b636941d9e2d64c5dbfdc
Miturralde <57236319+mikelitu369@users.noreply.github.com>
2022-04-03 11:53:02 +0200

    c

--- Assets/Scripts/PullFX.cs
+++ Assets/Scripts/PullFX.cs
@@ -7,23 +7,26 @@ public class PullFX : MonoBehaviour
     public enum FXs
     {
         explosionDisparo,
-        explosionDisparoPerf
+        explosionDisparoPerf,
+        expansion
     }
-    public static PullFX instance;
-    
+
+    public static PullFX instance;    
     private void Awake()
     {
         if (!instance) instance = this;
         else Destroy(this.gameObject);
     }
+
     [SerializeField] GameObject explosionDisparo;
     List<GameObject> explosionesDisparos = new List<GameObject>();
     [SerializeField] GameObject explosionDisparoPerf;
     List<GameObject> explosionesDisparosPerf = new List<GameObject>();
+    [SerializeField] GameObject ondaExpansiva;
+    List<GameObject> ondasExpansivas = new List<GameObject>();
 
     public GameObject NewExplosionDisparo(bool player2 = false)
     {
-
         GameObject explosion = null;
         if (explosionesDisparos.Count > 0)
         {
