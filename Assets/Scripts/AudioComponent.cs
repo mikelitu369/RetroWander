6162da93d309a94ed40c19faeaef38770d2021a5
Miturralde <57236319+mikelitu369@users.noreply.github.com>
2022-04-03 11:19:58 +0200

    son

---
+++ Assets/Scripts/AudioComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioComponent : MonoBehaviour
+{
+    private void Update()
+    {
+        if (!GetComponent<AudioSource>().isPlaying) Sonidero.instance.StopSound(this.gameObject);
+    }
+}
--- Assets/Scripts/Sonidero.cs
+++ Assets/Scripts/Sonidero.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class Sonidero : MonoBehaviour
 {
+    public static Sonidero instance;
+
+    private void Awake()
+    {
+        if (!instance) instance = this;
+        else Destroy(this.gameObject);
+    }
+
     [SerializeField] GameObject sonido;
-    List<GameObject> sonidos = new List<GameObject>();
+    List<AudioSource> sonidos = new List<AudioSource>();
+
+    public void NewSound(AudioClip audio)
+    {
+        if(sonidos.Count != 0)
+        {
+            sonidos[0].gameObject.SetActive(true);
+            sonidos[0].clip = audio;
+            sonidos[0].Play();
+            sonidos.Remove(sonidos[0]);
+        }
+        else
+        {
+            GameObject g = Instantiate(sonido, transform);
+            AudioSource audSou = g.GetComponent<AudioSource>();
+            audSou.clip = audio;
+            audSou.Play();
+        }
+    }
+
+    public void StopSound(GameObject audio)
+    {
+        sonidos.Add(audio.GetComponent<AudioSource>());
+        audio.SetActive(false);
+    }
 }
