61856a8df9bd9b84acbf79e7271a2081c0c7f7b4
Miturralde <57236319+mikelitu369@users.noreply.github.com>
2022-04-04 16:10:07 +0200

    escen

--- Assets/Scripts/HudHabilidades.cs
+++ Assets/Scripts/HudHabilidades.cs
@@ -7,19 +7,19 @@ public class HudHabilidades : MonoBehaviour
 {
     [SerializeField] NaveController nave;
 
-    Image[] iconos;
+    MaterialesHabilidades[] iconos;
 
     private void Start()
     {
-        iconos = transform.GetComponentsInChildren<Image>();
+        iconos = transform.GetComponentsInChildren<MaterialesHabilidades>();
     }
 
     private void Update()
     {
         for (int i = 0; i < 6; i++)
         {
-            iconos[i].fillAmount = nave.GetCharge(i);
-            iconos[i].GetComponent<MaterialesHabilidades>().Set(nave.GetCharge(i) >= 1);
+            iconos[i].GetComponent<Image>().fillAmount = nave.GetCharge(i);
+            iconos[i].Set(nave.GetCharge(i) >= 1);
         }
     }
 }
